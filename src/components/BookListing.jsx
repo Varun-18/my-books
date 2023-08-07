58cf783176c0b98d3606871c0088b7b8602681db
Varun-18 <varunshah735@gmail.com>
2023-08-07 12:38:11 +0530

    responsive commit

--- src/components/BookListing.jsx
+++ src/components/BookListing.jsx
@@ -1,10 +1,11 @@
 import { useRouter } from "next/router";
 import PropTypes from "prop-types";
 import BookCard from "./BookCard";
+import Filter from "./Filter";
 
 /**
  *  @parent : pages/index.js
- * 
+ *
  *  This component  recives the data in the form of array of objects and then maps them
  *
  * @param  books : Array of objects that contains the multiple books data that are mapped
@@ -14,15 +15,21 @@ import BookCard from "./BookCard";
 const BookListing = ({ books }) => {
   const router = useRouter();
   return (
-    <section className="text-gray-600 body-font">
-      <div className="container px-5 py-10 mx-auto">
-        <div className="flex flex-wrap -m-4 justify-center">
-          {books.map((item) => (
-            <BookCard item={item} key={item.id} />
-          ))}
-        </div>
+    <div>
+      <div className="text-right px-3 max-w-[1400px] pt-4">
+
+      <Filter />
       </div>
-    </section>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-3 pb-5 mx-auto">
+          <div className="flex flex-wrap -m-4 justify-center">
+            {books.map((item) => (
+              <BookCard item={item} key={item.id} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
---
+++ src/components/Filter.jsx
@@ -0,0 +1,80 @@
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+
+const Filter = () => {
+  const [show, setShow] = useState();
+  const router = useRouter();
+
+  const applyFilter = (filter) => {
+    router.push({
+      query: {
+        search: router.query.search ? router.query.search : "life",
+        pageID: 10,
+        filter,
+      },
+    });
+    setShow(false);
+  };
+
+  const clearFilter = () => {
+    router.push({
+      query: {
+        search: router.query.search ? router.query.search : "life",
+        pageID: 10,
+      },
+    });
+    setShow(false);
+  };
+  return (
+    <div className="sm:order-3 order-2">
+      <div className="relative ">
+        <button
+          className=" border-2  rounded-md p-2 right-0 top-1"
+          onClick={() => setShow(!show)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="#000"
+          >
+            <path
+              fill="#000"
+              fillRule="evenodd"
+              d="M20 5h-1.17a3.001 3.001 0 0 0-5.66 0H4a1 1 0 0 0 0 2h9.17a3.001 3.001 0 0 0 5.66 0H20a1 1 0 1 0 0-2zm-4 2a1 1 0 1 0 0-2 1 1 0 0 0 0 2zM3 12a1 1 0 0 1 1-1h1.17a3.001 3.001 0 0 1 5.66 0H20a1 1 0 1 1 0 2h-9.17a3.001 3.001 0 0 1-5.66 0H4a1 1 0 0 1-1-1zm5 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-4 4a1 1 0 1 0 0 2h9.17a3.001 3.001 0 0 0 5.66 0H20a1 1 0 1 0 0-2h-1.17a3.001 3.001 0 0 0-5.66 0H4zm13 1a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        {show ? (
+          <ul className=" absolute z-10 bg-white rounded-lg right-0  border-2 px-2  ">
+            <li
+              className="w-[100px] py-2 text-center cursor-pointer"
+              onClick={() => applyFilter("paid-ebooks")}
+              value={"paid-ebooks"}
+            >
+              paid ebooks
+            </li>
+            <hr />
+            <li
+              className="w-[100px] py-2 text-center cursor-pointer"
+              onClick={() => applyFilter("free-ebooks")}
+              value={"free-ebooks"}
+            >
+              free ebooks
+            </li>
+            <hr />
+            <li
+              className="w-[100px] py-2 text-center cursor-pointer"
+              onClick={() => clearFilter()}
+            >
+              clear filter
+            </li>
+          </ul>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
